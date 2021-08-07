7cc790c578bbe9c6db5222096d8f164513e1df40
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-08-07 12:41:16 +0900

    Update and rename dfs_bfs4 to dfs_bfs4.py

--- programmers/dfs_bfs4.py
+++ programmers/dfs_bfs4.py
@@ -25,8 +25,10 @@ def solution(tickets):
     recursive('ICN', tickets, [])
     return ANSWER
     
+"""
 # 한줄평
-global variable을 사용한 것이 아쉽기는 하지만 문제의 본질은 잘 파악한 것 같다.
+- global variable을 사용한 것이 아쉽기는 하지만 문제의 본질은 잘 파악한 것 같다.
+"""
 
 
 # 다른 풀이 1
@@ -45,5 +47,7 @@ def solution(tickets):
                 return rest
     return helper(tickets, ["ICN"])
 
+"""
 # 한줄평
-가장 깔끔한 풀이인 것 같다. 실패하는 탐색의 경우 result에 None을 할당하고, 최종 반환값을 정할 때는 None이 아닌 경우만 반환하도록 하는 점이 문제를 쉽게 해결한 열쇠인 것 같다. 하나의 재귀함수는 solution 함수 안에 선언하는 게 국룰인 것 같다.
+- 가장 깔끔한 풀이인 것 같다. 실패하는 탐색의 경우 result에 None을 할당하고, 최종 반환값을 정할 때는 None이 아닌 경우만 반환하도록 하는 점이 문제를 쉽게 해결한 열쇠인 것 같다. 하나의 재귀함수는 solution 함수 안에 선언하는 게 국룰인 것 같다.
+"""
