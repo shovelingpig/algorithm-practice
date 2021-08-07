474a347bb1bcd83f1b7d8c6bf46e8585a284b9aa
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-08-07 12:42:27 +0900

    Update and rename dp2 to dp2.py

--- programmers/dp2.py
+++ programmers/dp2.py
@@ -17,8 +17,10 @@ def solution(triangle):
     dfs(0, 0, 0)
     return MAX
 
+"""
 # 한줄평
-정확하지만 속도가 너무 느리다. 확인해보지 않아도 되는 경우를 제대로 걸러내지 못한 것 같다.
+- 정확하지만 속도가 너무 느리다. 확인해보지 않아도 되는 경우를 제대로 걸러내지 못한 것 같다.
+"""
 
 
 # 다른 풀이 1
@@ -32,10 +34,12 @@ def solution(triangle):
             else:
                 triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
     return max(triangle[-1])
-    
+
+"""
 # 한줄평
-재귀를 사용하지 않고 구현했다. 삼각형의 끝터리 부분과 안쪽 부분을 구별했고, 위의 값을 아래 값에 더해주는 과정을 반복하는 방식으로 문제를 해결하였다.
+- 재귀를 사용하지 않고 구현했다. 삼각형의 끝터리 부분과 안쪽 부분을 구별했고, 위의 값을 아래 값에 더해주는 과정을 반복하는 방식으로 문제를 해결하였다.
 출처) https://codedrive.tistory.com/49
+"""
 
 
 # 다른 풀이 2
@@ -58,15 +62,19 @@ def f(triangle, i, j, memo):
     memo[(i,j)] = x
 
     return x
-    
+   
+"""
 # 한줄평
-동적계획법의 Memoization 기법을 가장 잘 활용한 풀이 같다. 앞으로 나도 Memoization 기법을 구현할 때 구조를 잘 참고해야겠다.
+- 동적계획법의 Memoization 기법을 가장 잘 활용한 풀이 같다. 앞으로 나도 Memoization 기법을 구현할 때 구조를 잘 참고해야겠다.
+"""
 
 
 # 다른 풀이 3
 solution = lambda t, l = []: max(l) if not t else solution(t[1:], [max(x,y)+z for x,y,z in zip([0]+l, l+[0], t[0])])
 
+"""
 # 한줄평
 1) 한 층씩 제거하며, 그 층에서 계산한 최대 이동거리 배열을 계산하여, 한 층을 제거한 traingle을 첫번째 input, 이동거리 배열을 두 번째 input으로 넣어줍니다.  
 2) 따라서 traingle이 없으면 제거할 층이 없으므로 최종 조건입니다.  
-3) [0] + l, l + [0] 을 이용하여 모서리 조건을 해결해줍니다.  
+3) [0] + l, l + [0] 을 이용하여 모서리 조건을 해결해줍니다. 
+"""
