122962ecaf6dd240e8c37bcd88f62da7f12074e9
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-08-07 12:46:31 +0900

    Update and rename sort2 to sort2.py

--- programmers/sort2.py
+++ programmers/sort2.py
@@ -2,16 +2,20 @@
 def solution(numbers):
     return "0" if all([n == 0 for n in numbers]) else ''.join(sorted([str(n) for n in numbers], key=lambda s:s*3, reverse=True))
 
+"""
 # 한줄평
-numbers의 모든 숫자가 0인 경우가 핵심 코너 케이스였다. 이때 0000...이 아닌 0을 리턴했어야 하는데 이를 생각하는데 시간이 오래 걸렸다.
+- numbers의 모든 숫자가 0인 경우가 핵심 코너 케이스였다. 이때 0000...이 아닌 0을 리턴했어야 하는데 이를 생각하는데 시간이 오래 걸렸다.
+"""
 
 
 # 다른 풀이 1
 def solution(numbers):
     return  str(int(''.join(sorted([str(n) for n in numbers], key=lambda s:s*3, reverse=True))))
 
+"""
 # 한줄평
-내가 고민했던 코너 케이스를 더욱 아름답게 풀어낸 풀이이다. 문자열 0000...을 정수 0으로 바꾼 뒤 다시 문자열로 바꾸어주는 풀이이다.
+- 내가 고민했던 코너 케이스를 더욱 아름답게 풀어낸 풀이이다. 문자열 0000...을 정수 0으로 바꾼 뒤 다시 문자열로 바꾸어주는 풀이이다.
+"""
 
 
 # 다른 풀이 2
@@ -28,5 +32,7 @@ def solution(numbers):
     answer = str(int(''.join(n)))
     return answer
 
+"""
 # 한줄평
-number가 100보다 작다는 제약조건을 이용하지 않고 풀 수 있는 comparator를 사용한 가장 정석적인 풀이이다. 두 원소의 우선순위를 비교할 때 a+b와 b+a를 비교한 점이 인상깊었다.
+- number가 100보다 작다는 제약조건을 이용하지 않고 풀 수 있는 comparator를 사용한 가장 정석적인 풀이이다. 두 원소의 우선순위를 비교할 때 a+b와 b+a를 비교한 점이 인상깊었다.
+"""
