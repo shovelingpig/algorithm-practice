2242f750cac2e22564aa8350f4ba82f52946cf17
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-08-07 12:46:04 +0900

    Update and rename sort1 to sort1.py

--- programmers/sort1.py
+++ programmers/sort1.py
@@ -7,5 +7,7 @@ def solution(array, commands):
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
+"""
 # 한줄평
-본질적인 알고리즘 자체는 비슷했다. 하지만 list comprehension 대신 map과 labmda함수의 조합을 사용할 수도 있다는 사실을 깨달았다. 하지만 list comprehension이 더 빠르다고 한다.
+- 본질적인 알고리즘 자체는 비슷했다. 하지만 list comprehension 대신 map과 labmda함수의 조합을 사용할 수도 있다는 사실을 깨달았다. 하지만 list comprehension이 더 빠르다고 한다.
+"""
