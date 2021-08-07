eb386a5cbf2ea810959b6742d66afc8911dbb9d8
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-08-07 12:43:42 +0900

    Update and rename hash1 to hash1.py

--- programmers/hash1.py
+++ programmers/hash1.py
@@ -21,8 +21,10 @@ def solution(participant, completion):
     answer = dic[temp]
     return answer
 
+"""
 # 한줄평
-hash 함수를 이용하라는 문제의 취지에 가장 부합했던 풀이 같았다.
+- hash 함수를 이용하라는 문제의 취지에 가장 부합했던 풀이 같았다.
+"""
 
     
 # 다른 풀이 2
@@ -31,5 +33,7 @@ def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
 
+"""
 # 한줄평
-collections 라이브러리의 Counter 객체에서 - operator가 하는 역할을 처음 알게 되었다.
+- collections 라이브러리의 Counter 객체에서 - operator가 하는 역할을 처음 알게 되었다.
+"""
